dfe372080eb13e0f2c1150ec15dd1e1dd1b5a445
Tony F. <a20223209@pucp.edu.pe>
2025-04-25 21:37:14 -0500

    Pregunta 2

--- Implementacion/src/main/java/gtics/implementacion/Repository/HospitalRepository.java
+++ Implementacion/src/main/java/gtics/implementacion/Repository/HospitalRepository.java
@@ -1,6 +1,8 @@
 package gtics.implementacion.Repository;
 import gtics.implementacion.Entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+
 }
--- Implementacion/src/main/java/gtics/implementacion/Repository/PacienteRepository.java
+++ Implementacion/src/main/java/gtics/implementacion/Repository/PacienteRepository.java
@@ -2,5 +2,10 @@ package gtics.implementacion.Repository;
 import gtics.implementacion.Entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
+
+    List<Paciente> findByHospital_IdHospital(Integer idHospital);
+
 }
